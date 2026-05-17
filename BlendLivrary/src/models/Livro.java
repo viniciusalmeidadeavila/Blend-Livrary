238b73ae923960df945e9afe877e2c7ccec2b706
Vinicius <viniciusalmeidadeavila@gmail.com>
2026-05-17 11:33:30 -0300

    feat: adicionando Classe Livro e Herança da Classe Livro

---
+++ BlendLivrary/src/models/Livro.java
@@ -0,0 +1,33 @@
+package models;
+
+// ABSTRAÇÃO
+public abstract class Livro {
+    // ENCAPSULAMENTO
+    private int id;
+    private String titulo;
+    private String autor;
+
+    public Livro(int id, String titulo, String autor) {
+        this.id = id;
+        this.titulo = titulo;
+        this.autor = autor;
+    }
+
+    public String getTitulo(){
+        return this.titulo;
+    }
+
+    public void setTitulo(String titulo){
+        this.titulo = titulo;
+    }
+
+    public String getAutor(){
+        return this.autor;
+    }
+
+    public void setAutor(String autor){
+        this.autor = autor;
+    }
+
+    public abstract String obterDetalhes();
+}
